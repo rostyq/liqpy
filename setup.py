4fd93ae8df22fd7a3c032b2769cd4d8b5808ebc8
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2023-10-24 18:27:00 +0300

    add pyproject and setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from tomllib import load
+
+
+with open("pyproject.toml") as f:
+    project = load(f)["project"]
+
+    setup(
+        name=project["name"],
+        version=project["version"],
+        description=project["description"],
+        keywords=project["keywords"],
+        author=project["authors"][0]["name"],
+        author_email=project["authors"][0]["email"],
+        url=project["urls"]["Homepage"],
+        packages=[project["name"]],
+        install_requires=project["dependencies"],
+        python_requires=">=3.11",
+    )
